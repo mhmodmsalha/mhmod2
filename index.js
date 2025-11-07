86b41ed0efc3cc61ee1062e09a2b04b82723695f
mhmodmsalha <mhmodmsalha55@gmail.com>
2025-11-07 08:59:55 +0200

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = 3500;
+  
+
+
+
+
+
+
+
+app.listen(port,()=>(console.log('http://localhost:$(port);')))
