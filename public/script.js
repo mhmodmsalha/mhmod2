b29e15eaabdc485bcd1581c73e4661a2b36c2591
mhmodmsalha <mhmodmsalha55@gmail.com>
2026-02-02 02:46:27 +0200

    add greeting and catgory delete confirmation

--- public/script.js
+++ public/script.js
@@ -1,2 +1,24 @@
-let greating = "صباح الخير"
+
+let greating = "صباح الخير";
 document.getElementById('greating').innerHTML = greating;
+
+
+async function deleteCategory(id) {
+    
+    if (confirm("Are you sure you want to delete this category and all its tasks?")) {
+        try {
+            const res = await fetch(`/categories/delete/${id}`, {
+                method: 'DELETE'
+            });
+
+            if (res.ok) {
+                alert("Deleted successfully!");
+                location.reload(); 
+            } else {
+                alert("Error deleting category");
+            }
+        } catch (err) {
+            console.error("Failed to delete:", err);
+        }
+    }
+}
